f99ae77717b4ceef2fa79c8da11c41602c8b91db
mgd <mgd@posteo.net>
2024-11-23 22:58:57 +0000

    added .gitignore

---
+++ src/main/java/com/mg/shakespeareapi/ShakespeareApiApplication.java
@@ -0,0 +1,13 @@
+package com.mg.shakespeareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakespeareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakespeareApiApplication.class, args);
+	}
+
+}
